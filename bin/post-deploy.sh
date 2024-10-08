a9d4ed185e7163d115aaaa969a83ef9bf73ba4ce
Chris Continanza <christopher.continanza@gmail.com>
2024-10-08 11:41:36 -0400

    provision synchronously for now

--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-curl -X POST $GRAX_ADMIN_API_URL/configure-datalake \
+curl --max-time 1800 -X POST $GRAX_ADMIN_API_URL/configure-datalake \
   --data-urlencode bucket_name=$S3_BUCKET_NAME \
   --data-urlencode bucket_region=$S3_AWS_REGION \
   --data-urlencode bucket_key_id=$S3_AWS_ACCESS_KEY_ID \
